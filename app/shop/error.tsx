a49281c93e7a75b05a5145c5911507dd020f3897
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-11-07 00:15:38 +0900

    feature: Handling errors

---
+++ app/shop/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+
+export default function error() {
+  return (
+    <main>
+      <h1>An error occurred!</h1>
+      <p>failed to fetch items data. Please try again later.</p>
+    </main>
+  );
+}
