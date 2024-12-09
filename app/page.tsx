1b5f8e0b81be0fd53c3732fa279f44fa85b056eb
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-12-08 22:54:02 -0800

    style: fix Collection layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image';
-
-import imageCollection from '@/public/images/main_section2_banner.jpg';
-import imageCollection2 from '@/public/images/main_section2_banner2.jpg';
 import Layout from '@/components/Layout/Layout';
 import MainBanner from '@/components/Home/MainBanner';
 import Collection from '@/components/Home/Collection';
