4ba3ebb1105104d1b01f81cd3ba9acb02ebb1a60
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-12-12 21:36:16 -0800

    feature: add Items in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@ import Layout from '@/components/Layout/Layout';
 import MainBanner from '@/components/Home/MainBanner';
 import Collection from '@/components/Home/Collection';
 import ItemsGrid from '@/components/Home/ItemsGrid';
+import { getItems } from '@/lib/items';
+import { Suspense } from 'react';
+
+async function Items() {
+  const items = await getItems();
+  return <ItemsGrid items={items} />;
+}
 
 export default function Home() {
   return (
     <Layout>
       <MainBanner />
       <Collection />
-      <ItemsGrid />
+      <Suspense fallback={<div>Loading..</div>}>
+        <Items />
+      </Suspense>
     </Layout>
   );
 }
--- components/Home/ItemsGrid.tsx
+++ components/Home/ItemsGrid.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Item from '../Item/Item';
 import styles from './ItemsGrid.module.scss';
+import BestItem from '../Item/BestItem';
 
-export default function ItemsGrid() {
+export default function ItemsGrid({ items }: { items: any }) {
   return (
     <div className={styles.wrap}>
-      <div>
-        <div>New Arrivals</div>
-        <div>New Arrivals</div>
-      </div>
+      <span>New Arrivals</span>
+      <ul className={styles.item_wrap}>
+        {items.map((item: any) => {
+          return (
+            <li key={item.id} className={styles.item}>
+              <BestItem {...item} />
+            </li>
+          );
+        })}
+      </ul>
+      <span>New Arrivals</span>
     </div>
   );
 }
