6da4ee7bbe5a3a26e7170cca4bf61714d6e278b1
Eunji <103646256+ejk0731@users.noreply.github.com>
2025-01-04 23:32:32 -0800

    fix: Swiper error

--- components/Home/LookBooks/LookBookSlider.tsx
+++ components/Home/LookBooks/LookBookSlider.tsx
@@ -1,16 +1,18 @@
+'use client';
 import React from 'react';
 import LookBookSlide from './LookBookSlide';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function LookBookSlider() {
   const item = [1, 2, 3, 4];
   return (
     <div>
-      {/* <Swiper>
+      <Swiper spaceBetween={50} slidesPerView={3}>
         {item.map(index => {
-          return <SwiperSlide>index</SwiperSlide>;
+          return <SwiperSlide>{index}</SwiperSlide>;
         })}
-      </Swiper> */}
+      </Swiper>
     </div>
   );
 }
