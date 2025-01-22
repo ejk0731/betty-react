ef167b8d5ccf3a33dda07499c96470ea193bb4b6
Eunji <103646256+ejk0731@users.noreply.github.com>
2025-01-21 20:53:43 -0800

    feature: BtnHamburger - add transition

--- components/Header/BtnHamburger.tsx
+++ components/Header/BtnHamburger.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './BtnHamburger.module.scss';
 
 export default function BtnHamburger() {
+  const [showCloseBtn, setShowCloseBtn] = useState<boolean>(false);
   return (
-    <button type="button" className={styles.btn_hamburger}>
+    <button
+      type="button"
+      className={`${styles.btn_hamburger} ${showCloseBtn && styles.btn_close}`}
+      onClick={() => setShowCloseBtn(!showCloseBtn)}
+    >
       <span className={styles.line}></span>
       <span className={styles.line}></span>
       <span className={styles.line}></span>
