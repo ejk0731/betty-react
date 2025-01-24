47351c6feb0e45dd77540faaa2340846b3501af9
Eunji <103646256+ejk0731@users.noreply.github.com>
2025-01-23 20:55:45 -0800

    feature: Header - add nav onClick event

--- components/Header/NavLink.tsx
+++ components/Header/NavLink.tsx
@@ -1,19 +1,25 @@
 'use client';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import React from 'react';
 import styles from './NavLink.module.scss';
 
 export default function NavLink({
   href,
   children,
+  onClick,
 }: {
   href: string;
   children: React.ReactNode;
+  onClick: (e: React.MouseEvent) => void;
 }) {
   return (
     <>
-      <Link href={href} scroll={false} className={styles.link}>
+      <Link
+        href={href}
+        scroll={false}
+        className={styles.link}
+        onClick={onClick}
+      >
         {children}
       </Link>
     </>
